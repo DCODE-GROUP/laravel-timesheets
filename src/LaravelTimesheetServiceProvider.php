eaa255e1c6cebc715e051d0fe7bfa836caddee10
Nathan Rzepecki <nathan@lionslair.net.au>
2022-11-30 16:39:03 +0800

    catch the connection and table error

--- src/LaravelTimesheetServiceProvider.php
+++ src/LaravelTimesheetServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Dcodegroup\LaravelTimesheet;
 
 use Dcodegroup\LaravelTimesheet\Commands\InstallCommand;
+use Exception;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -34,21 +35,26 @@ class LaravelTimesheetServiceProvider extends ServiceProvider
      */
     protected function offerPublishing()
     {
-        if (! Schema::hasTable('timesheets') && ! class_exists('CreateTimesheetsTable')) {
-            $timestamp = date('Y_m_d_His', time());
+        try {
+            if (! class_exists('CreateTimesheetsTable') && ! Schema::hasTable('timesheets')) {
+                $timestamp = date('Y_m_d_His', time());
 
-            $this->publishes([
-                                 __DIR__ . '/../database/migrations/create_timesheets_table.php.stub.php' => database_path('migrations/' . $timestamp . '_create_timesheets_table.php'),
-                             ], 'laravel-timesheets-migrations');
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_timesheets_table.php.stub.php' => database_path('migrations/'.$timestamp.'_create_timesheets_table.php'),
+                ], 'laravel-timesheets-migrations');
+            }
+        } catch (Exception $e) {
+            report($e);
         }
+
     }
 
     protected function registerCommands()
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
-                                InstallCommand::class,
-                            ]);
+                InstallCommand::class,
+            ]);
         }
     }
 }
